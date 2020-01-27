8a5a8dbbedb25187982e8c0ddd723636d36b9f52
sunil chaulagain <mesunil2010@gmail.com>
2020-01-27 19:33:36 +1100

    Updates

---
+++ precache-manifest.03b9903322bd34c4b2b38958f931ec2b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71a5e91885bf0d0c622feafdbc125c5",
+    "url": "/loanguru/index.html"
+  },
+  {
+    "revision": "24ce4dab720158ba08b0",
+    "url": "/loanguru/static/css/main.6cbe20dd.chunk.css"
+  },
+  {
+    "revision": "eace20b1a850f2f4a624",
+    "url": "/loanguru/static/js/2.25b24a23.chunk.js"
+  },
+  {
+    "revision": "24ce4dab720158ba08b0",
+    "url": "/loanguru/static/js/main.db209b15.chunk.js"
+  },
+  {
+    "revision": "7c66b8b6983e41f91f20",
+    "url": "/loanguru/static/js/runtime~main.b1fd0f9f.js"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/loanguru/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/loanguru/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "f7cb2d390498c2969b2b1050f3e024ce",
+    "url": "/loanguru/static/media/creativetim-white-slim.f7cb2d39.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/loanguru/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/loanguru/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/loanguru/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/loanguru/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/loanguru/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/loanguru/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/loanguru/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/loanguru/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/loanguru/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ba449a0697388668ef05e043ffdeafca",
+    "url": "/loanguru/static/media/img-2-1200x1000.ba449a06.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/loanguru/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/loanguru/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/loanguru/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/loanguru/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/loanguru/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/loanguru/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/loanguru/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/loanguru/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/loanguru/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/loanguru/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
